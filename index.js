6651788af3b22173129d9342c82f97da9e8730a2
Dilshan Gamage <randikasandaruwan917@gmail.com>
2025-03-05 03:03:07 +0530

    index.js updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const PORT = process.env.PORT | 3000;
 
 const CustomerRoute = require('./route/CustomerRoute');
 const OrderRoute = require('./route/OrderRoute');
@@ -12,8 +13,18 @@ const UserRoute = require('./route/UserRoute');
 const app = express();
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/pos_db').then(()=>{
+    console.log('db connected');
+}).catch(e=>{
+    console.log(e);
+})
+
 app.use('/api/v1/customers', CustomerRoute);
 app.use('/api/v1/orders', OrderRoute);
 app.use('/api/v1/payments', PaymentRout);
 app.use('/api/v1/products', ProductRoute);
 app.use('/api/v1/users', UserRoute);
+
+app.listen(PORT, ()=>{
+    console.log(`server started on port ${PORT}`)
+});
