79cde4521e8f7b6b206bbc4a19de2b28de4b657d
Dilshan Gamage <randikasandaruwan917@gmail.com>
2025-03-06 01:27:35 +0530

    OrderRoute set

--- route/OrderRoute.js
+++ route/OrderRoute.js
@@ -2,4 +2,11 @@ const express = require('express');
 const router = express.Router();
 const OrderController = require('../controller/OrderController');
 
+router.post('/create',OrderController.saveOrder);
+router.put('/update/:id',OrderController.updateOrder);
+router.put('/update-status/:id',OrderController.updateOrderStatus);
+router.delete('/delete/:id',OrderController.deleteOrder);
+router.get('/find/:id',OrderController.findOrder);
+router.get('/find-all',OrderController.loadAllOrders);
+
 module.exports = router;
