bf0543f13511aea44308d882d6298224ecddb9eb
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-08 12:35:28 +0530

    PaymentController update

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -1,4 +1,61 @@
 const Payment = require('../model/PaymentSchema');
-module.exports={
 
+const makePayment = async (req,resp)=>{  // admin, manager
+    try{
+            const createdPayment = new Payment(req.body);
+            const savedPayment = await createdPayment.save();
+            resp.status(201).json({message:"Payment saved",data:savedPayment});
+        }catch(e){
+            resp.status(500).json({error:e.message})
+        }
+}
+
+const findIncomeToday = async (req,resp)=>{  // admin, manager
+    try{
+
+            const {day} = req.query; // YYYY-MM-DD
+            const startDay = new Date(day);
+            const endDay = new Date(day);
+
+            endDay.setDate(endDay.getDate()+1);
+            const data = await Payment.find({
+                Date:{
+                    $gte:startDay,
+                    $lt:endDay
+                }
+            });
+            const totalIncome = data.reduce((sum, payment)=>sum+payment.amount,0);
+            resp.status(200).json({message:"today income",data:totalIncome});
+        }catch(e){
+            resp.status(500).json({error:e.message})
+        }
+}
+
+const findIncomeByCurrentMonth = async (req,resp)=>{  // admin, manager
+    try{
+
+            const now = new Date(); 
+            const startOfMonth = new Date(now.getFullYear, now.getMonth(), 1);
+            const endOfMonth = new Date(now.getFullYear, now.getMonth()+1, 1);
+
+            
+            const data = await Payment.find({
+                Date:{
+                    $gte:startOfMonth,
+                    $lt:endOfMonth
+                }
+            });
+            const incomeByDay = data.reduce((acc, payment)=>{
+                const day = payment.Date.toISOString().split("T")[0];
+                acc[day] = (acc[day] || 0)+payment.amount;
+                return acc;
+            },{});
+            resp.status(200).json({message:"month income",data:{month:now.getMonth()+1, income:incomeByDay}});
+        }catch(e){
+            resp.status(500).json({error:e.message})
+        }
+}
+
+module.exports={
+    makePayment, findIncomeToday, findIncomeByCurrentMonth
 };
