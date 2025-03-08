149b8b52678c91f30293261400b8d8fe5cf9216f
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-08 12:41:14 +0530

    Update PaymentRout.js

--- route/PaymentRout.js
+++ route/PaymentRout.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const PaymentController = require('../controller/PaymentController');
 
+router.post('/create',PaymentController.makePayment);
+router.put('/income-by-day',PaymentController.findIncomeToday);
+router.put('/income-by-month',PaymentController.findIncomeByCurrentMonth);
+
 module.exports = router;
